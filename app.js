4bb2662a72e7d12466d6492f3051bd8661c29869
Emre MUTLU <mutluemre93@gmail.com>
2021-01-11 19:34:45 +0300

    CORS ayarları

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,19 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// API'ımız api.natours.com adresinde iken frontend kısmından erişilmesi için natours.com adresine izin veriyoruz, alttaki komutla
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com',
+//   })
+// );
+
+app.options('*', cors()); // Tüm optionlara izin verdik, tüm pathlere
+app.options('/api/v1/tours/:id', cors()); // Tek path'e izin verdik
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
